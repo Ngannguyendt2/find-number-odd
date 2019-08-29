590b30d6a1175cf861dedd102c94b2a951bb2b0d
Ngannguyendt2 <ngannguyendt2haui@gmail.com>
2019-08-29 10:09:44 +0700

    repair check number

--- main.js
+++ main.js
@@ -1,41 +1,33 @@
-let array = [235, -439, 124, 678, 357, 573];
-let num1 = [];
-let num2 = [];
-let num3 = [];
-let num4 = [];
+let array = [235, -439, 1357,124, 678, 357, 573];
+let str = '';
 let array1 = [];
-let max = 0;
 
-function insertNumber() {
+function changeString() {
     for (let i = 0; i < array.length; i++) {
-        num1[i] = array[i] % 100;
-        num2[i] = Math.floor(array[i] / 100);
-        num3[i] = num1[i] % 10;
-        num4[i] = Math.floor(num1[i] / 10);
+        str = array[i].toString();
+        check();
     }
-}
-
-insertNumber();
-
-function check() {
-    for (let i = 0; i < array.length; i++) {
-        if ((num3[i] % 2) != 0 && (num4[i] % 2) != 0 && (num2[i] % 2) != 0) {
-            array1.push(array[i]);
-        }
-    }
-    return array1;
-}
-
-function findMax() {
-    check();
-    max = array1[0];
+    let max = array1[0];
     for (let i = 0; i < array1.length; i++) {
         if (array1[i] > max) {
             max = array1[i];
         }
-
     }
-    document.write("Max is: " + max);
+    return document.write("Max is " + max);
 }
+
+function check() {
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+        if ((str[i] % 2) != 0) {
+            count++;
+        }
+    }
+    if (count == str.length) {
+        array1.push(Number(str));
+    }
+}
+
+changeString();
 
 
